06c8502f78a7b8f6d9bf47b456e69e1e24293cd0
blackbear <98885273+blackbear216@users.noreply.github.com>
2023-08-19 13:38:10 +0100

    Add square highlighting when mouseover

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ function initializeGrid() {
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
         div.classList.toggle('box');
+        div.addEventListener('mouseover', () => {
+            div.setAttribute('style', 'background: grey;');
+        })
+        div.addEventListener('mouseleave', () => {
+            div.setAttribute('style', 'background: white;');
+        })
         container.appendChild(div);
     }
 }
 
 initializeGrid()
+
