bb930c056bf8c73d72286ac18f9ec3dfe00efd4d
blackbear <98885273+blackbear216@users.noreply.github.com>
2023-08-19 13:26:05 +0100

    Change grid func to toggle classlist, not setattr

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function initializeGrid() {
     const container = document.querySelector('.container');
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
-        div.setAttribute('style', 'width: 8px; height: 8px; background: black;');
+        div.classList.toggle('box');
         container.appendChild(div);
     }
 }
