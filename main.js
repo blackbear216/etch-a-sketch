6e5944ba726e0db21f9c1db8151745549b5aaefd
blackbear <98885273+blackbear216@users.noreply.github.com>
2023-08-21 10:47:25 +0100

    Add erasing functionality

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
 function initializeGrid() {
     const container = document.querySelector('.container');
+
     let zDown = false;
+    let eDown = false;
+
+    // handles drawing
+    document.addEventListener('keydown', (e) => {
+        if (e.code == 'KeyZ') {
+            zDown = true
+        }
+    })
+    document.addEventListener('keyup', (e) => {
+        if (e.code == 'KeyZ') {
+            zDown = false
+        }
+    })
+
+    // handles erasing
+    document.addEventListener('keydown', (e) => {
+        if (e.code == 'KeyE') {
+            eDown = true
+        }
+    })
+    document.addEventListener('keyup', (e) => {
+        if (e.code == 'KeyE') {
+            eDown = false
+        }
+    })
+
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
         div.classList.toggle('box');
 
-        document.addEventListener('keydown', (e) => {
-            if (e.code == 'KeyZ') {
-                zDown = true
-            }
-        })
-
-        document.addEventListener('keyup', (e) => {
-            if (e.code == 'KeyZ') {
-                zDown = false
-            }
-        })
-    
         div.addEventListener('mouseover', () => {
             div.classList.toggle('hover');
+            // if Z down, draw in square
             if (zDown == true) {
                 div.classList.add('filled');
+            // if E down, erase in square
+            } else if (eDown == true) {
+                div.classList.remove('filled');
             }
         })
         div.addEventListener('mouseleave', () => {
@@ -29,6 +48,14 @@ function initializeGrid() {
 
         container.appendChild(div);
     }
+}
+
+function initializeDrawingFunctionality() {
+
+}
+
+function initializeErasingFunctionality() {
+
 }
 
 initializeGrid()
