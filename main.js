2c6f199946b4138c2173251dd1d36271f2ab7655
blackbear <98885273+blackbear216@users.noreply.github.com>
2023-08-21 09:51:17 +0100

    Add 'z' key to turn on and off drawing mode

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
 function initializeGrid() {
     const container = document.querySelector('.container');
+    let zDown = false;
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
         div.classList.toggle('box');
+
+        document.addEventListener('keydown', (e) => {
+            if (e.code == 'KeyZ') {
+                zDown = true
+            }
+        })
+
+        document.addEventListener('keyup', (e) => {
+            if (e.code == 'KeyZ') {
+                zDown = false
+            }
+        })
+    
         div.addEventListener('mouseover', () => {
             div.classList.toggle('hover');
+            if (zDown == true) {
+                div.classList.add('filled');
+            }
         })
         div.addEventListener('mouseleave', () => {
             div.classList.toggle('hover');
         })
-        div.addEventListener('mousedown', () => {
-            div.classList.toggle('filled');
-        })
+
         container.appendChild(div);
     }
 }
 
 initializeGrid()
-
